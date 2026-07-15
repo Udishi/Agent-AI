5365b8ac70933744cb0b6d700bca6d6f691810b5
Udishi <udishi2006@gmail.com>
2026-07-15 13:05:02 +0530

    Create memory.py

---
+++ memory.py
@@ -0,0 +1,16 @@
+class Memory:
+
+    def __init__(self):
+
+        self.history = []
+
+    def add(self, role, content):
+
+        self.history.append({
+            "role": role,
+            "content": content
+        })
+
+    def get(self):
+
+        return self.history
